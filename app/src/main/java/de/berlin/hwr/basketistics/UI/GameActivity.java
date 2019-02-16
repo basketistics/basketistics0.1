031241f4c3d754c82c37152066240388b4d4105a
Jona <s_heitzer@stud.hwr-berlin.de>
2019-02-16 20:32:14 +0100

    WÖRKS. Proper list item layout still missing.

--- app/src/main/java/de/berlin/hwr/basketistics/UI/GameActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/GameActivity.java
@@ -1,6 +1,7 @@
 package de.berlin.hwr.basketistics.UI;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -283,5 +284,9 @@ public class GameActivity extends AppCompatActivity {
         attachButtonsToViewModel();
         attachTextViewsToViewModel();
         initMockDB();
+
+        // For testing
+        Intent intent = new Intent(this, TeamActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/de/berlin/hwr/basketistics/UI/TeamAdapter.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/TeamAdapter.java
@@ -2,6 +2,7 @@ package de.berlin.hwr.basketistics.UI;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import de.berlin.hwr.basketistics.R;
 
 public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder> {
+
+    private static final String TAG = "TeamAdapter";
 
     private String[] teamDataset;
 
@@ -30,14 +33,15 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
     @Override
     public TeamViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.team_list_item, parent, false);
-
-        return null;
+        TeamViewHolder teamViewHolder = new TeamViewHolder(textView);
+        return teamViewHolder;
     }
 
     // Replaces the contents of a view (invoked by layout manager)
     @Override
     public void onBindViewHolder(@NonNull TeamViewHolder teamViewHolder, int i) {
         teamViewHolder.textView.setText(teamDataset[i]);
+        Log.e(TAG, "onBindViewHolder was entered.");
     }
 
     @Override
