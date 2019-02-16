5be6cef8fb5c4d6016e1fce689826e20bc9033c6
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-02-16 23:48:45 +0100

    Works.

--- app/src/main/java/de/berlin/hwr/basketistics/Persistency/MockPlayerDB.java
+++ app/src/main/java/de/berlin/hwr/basketistics/Persistency/MockPlayerDB.java
@@ -5,24 +5,28 @@ import java.util.List;
 
 public class MockPlayerDB {
 
-    class Entry {
-        int playerID = 0;
-        String playerName = "";
-        int playerNumber = 0;
+    public class Entry {
+        public int playerID = 0;
+        public String playerName = "";
+        public int playerNumber = 0;
+        public String playerDescription = "";
 
-        public Entry(int playerID, String playerName, int playerNumber) {
-
+        public Entry(int playerID, String playerName, int playerNumber, String playerDescription) {
+            this.playerID = playerID;
+            this.playerName = playerName;
+            this.playerNumber = playerNumber;
+            this.playerDescription = playerDescription;
         }
     }
 
-    List<Entry> db = new ArrayList<Entry>();
+    public List<Entry> db = new ArrayList<Entry>();
 
     public MockPlayerDB() {
-        this.db.add(new Entry(0, "Player1", 1));
-        this.db.add(new Entry(1, "Player2", 2));
-        this.db.add(new Entry(2, "Player3", 3));
-        this.db.add(new Entry(3, "Player4", 4));
-        this.db.add(new Entry(4, "Player5", 5));
-        this.db.add(new Entry(999, "Trainer", 999));
+        this.db.add(new Entry(0, "Player1", 5, "Eveniet tempora sint et et. Nobis illo qui aut quas."));
+        this.db.add(new Entry(1, "Player2", 12, "Non illum fugiat recusandae magnam quod molestiae."));
+        this.db.add(new Entry(2, "Player3", 3, "Minima ut vitae ut consequatur quis vel."));
+        this.db.add(new Entry(3, "Player4", 24, "Necessitatibus minima maxime voluptatem ut possimus consequuntur."));
+        this.db.add(new Entry(4, "Player5", 8, "Officiis dignissimos qui id ut eligendi."));
+        this.db.add(new Entry(999, "Trainer", 999, "Quam est dolor consequatur sunt vero autem accusantium."));
     }
 }
--- app/src/main/java/de/berlin/hwr/basketistics/UI/TeamAdapter.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/TeamAdapter.java
@@ -4,9 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import de.berlin.hwr.basketistics.Persistency.MockPlayerDB;
 import de.berlin.hwr.basketistics.R;
 
 public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder> {
@@ -15,13 +19,24 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
 
     private String[] teamDataset;
 
+    // TODO: Only for testing.
+    public MockPlayerDB mockPlayerDB = new MockPlayerDB();
+
     public static class TeamViewHolder extends RecyclerView.ViewHolder {
-        // Example with Strings
-        // TODO: Change.
+
+        private ImageView playerImageView;
+        private TextView playerName;
+        private TextView playerNumber;
+        private TextView playerDescription;
+
         public TextView textView;
-        public TeamViewHolder(TextView v) {
-            super(v);
-            textView = v;
+        public TeamViewHolder(View itemView) {
+            super(itemView);
+            playerImageView = (ImageView) itemView.findViewById(R.id.playerPicture);
+            playerName = (TextView) itemView.findViewById(R.id.playerNameTextView);
+            playerNumber = (TextView) itemView.findViewById(R.id.playerNumber);
+            playerDescription = (TextView) itemView.findViewById(R.id.playerDescription1);
+
         }
     }
 
@@ -32,16 +47,19 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
     @NonNull
     @Override
     public TeamViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.team_list_item, parent, false);
-        TeamViewHolder teamViewHolder = new TeamViewHolder(textView);
+        LinearLayout playerListItem = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.team_list_item, parent, false);
+        TeamViewHolder teamViewHolder = new TeamViewHolder(playerListItem);
         return teamViewHolder;
     }
 
     // Replaces the contents of a view (invoked by layout manager)
     @Override
     public void onBindViewHolder(@NonNull TeamViewHolder teamViewHolder, int i) {
-        teamViewHolder.textView.setText(teamDataset[i]);
         Log.e(TAG, "onBindViewHolder was entered.");
+        // TODO: teamViewHolder.playerImageView.setImageDrawable();
+        teamViewHolder.playerName.setText(mockPlayerDB.db.get(i).playerName);
+        teamViewHolder.playerNumber.setText("" + mockPlayerDB.db.get(i).playerNumber);
+        teamViewHolder.playerDescription.setText(mockPlayerDB.db.get(i).playerDescription);
     }
 
     @Override
