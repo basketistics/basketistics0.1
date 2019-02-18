d9fedba51a6179ea0356df6bf4d0923d7e457706
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-02-18 14:00:27 +0100

    Added MockDBTeamObserver.

---
+++ app/src/main/java/de/berlin/hwr/basketistics/Persistency/MockDBTeamObserver.java
@@ -0,0 +1,22 @@
+package de.berlin.hwr.basketistics.Persistency;
+
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+
+import de.berlin.hwr.basketistics.ViewModel.TeamViewModel;
+
+public class MockDBTeamObserver implements Observer {
+
+    private TeamViewModel teamViewModel;
+    private MockPlayerDB mockPlayerDB;
+
+    public MockDBTeamObserver(TeamViewModel teamViewModel, MockPlayerDB mockPlayerDB) {
+        this.teamViewModel = teamViewModel;
+        this.mockPlayerDB = mockPlayerDB;
+    }
+
+    @Override
+    public void onChanged(@Nullable Object o) {
+
+    }
+}
