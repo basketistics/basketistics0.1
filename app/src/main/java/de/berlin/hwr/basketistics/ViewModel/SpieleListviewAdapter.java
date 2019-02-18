4114f983cb98d56672dbc9ea1b64da6ca8b99387
Hendrik <h.hintze@gmx.de>
2019-02-18 21:53:28 +0100

    still debugging adapter adapter

--- app/src/main/java/de/berlin/hwr/basketistics/ViewModel/SpieleListviewAdapter.java
+++ app/src/main/java/de/berlin/hwr/basketistics/ViewModel/SpieleListviewAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -22,16 +23,36 @@ public class SpieleListviewAdapter extends ArrayAdapter<SpieleListViewItem> {
         this.kontext = kontext;
     }
 
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         View v = convertView;
+
         if(v == null){
             LayoutInflater lif = LayoutInflater.from(kontext);
             v = lif.inflate(resourceLayout, null);
         }
+
         SpieleListViewItem slvi = getItem(position);
 
+        if(slvi != null){
+            TextView date = v.findViewById(R.id.date);
+            TextView time = v.findViewById(R.id.time);
+            TextView heim = v.findViewById(R.id.heim);
+            TextView heim_score = v.findViewById(R.id.heim_score);
+            TextView gast = v.findViewById(R.id.gast);
+            TextView gast_score = v.findViewById(R.id.gast_score);
+            TextView ort = v.findViewById(R.id.ort);
+
+            date.setText(slvi.getDate());
+            if(time != null){time.setText(slvi.getZeit());}
+            if(heim != null){heim.setText(slvi.getHeim());}
+            if(heim_score != null){heim_score.setText(slvi.getPunkte_heim());}
+            if(gast != null){gast.setText(slvi.getGast());}
+            if(gast_score != null){gast_score.setText(slvi.getPunkte_gast());}
+            if(ort != null){ort.setText(slvi.getOrt());}
+
+        }
 
         return v;
     }
