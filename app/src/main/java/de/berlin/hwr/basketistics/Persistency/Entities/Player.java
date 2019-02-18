8e2dcbfa35122499174e1fa3450592c37670d2de
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-02-18 14:01:34 +0100

    Added id to Player

--- app/src/main/java/de/berlin/hwr/basketistics/Persistency/Entities/Player.java
+++ app/src/main/java/de/berlin/hwr/basketistics/Persistency/Entities/Player.java
@@ -7,11 +7,13 @@ import java.util.List;
 // TODO: Annotate as room entity.
 public class Player {
 
+    private int id;
     private String name;
     private int number;
     private String description;
 
-    public Player(String name, int number, String description) {
+    public Player(int id, String name, int number, String description) {
+        this.id = id;
         this.name = name;
         this.number = number;
         this.description = description;
