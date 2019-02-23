a24a9fe1f5bab3d82dbc5e9405821d3636bd25fa
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-02-23 23:54:22 +0100

    Problem seems to be really fixed now by making not final and pubilc to whole TeamActivity. Also got rid of trash.

--- app/src/main/java/de/berlin/hwr/basketistics/UI/TeamActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/TeamActivity.java
@@ -1,11 +1,8 @@
 package de.berlin.hwr.basketistics.UI;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,18 +14,15 @@ import android.widget.Button;
 
 import java.util.List;
 
-import de.berlin.hwr.basketistics.Persistency.Entities.Player;
 import de.berlin.hwr.basketistics.Persistency.Entities.PlayerEntity;
 import de.berlin.hwr.basketistics.R;
 import de.berlin.hwr.basketistics.ViewModel.TeamDBViewModel;
-import de.berlin.hwr.basketistics.ViewModel.TeamViewModel;
-
 public class TeamActivity extends AppCompatActivity {
+
+    private TeamAdapter teamAdapter;
 
     private final static int ADD_PLAYER_ACTIVITY_REQUEST_CODE = 3;
     private static final String TAG = "AddPlayerActivity";
-
-    private TeamViewModel teamViewModel;
 
     private Button addPlayerButton;
     private TeamDBViewModel teamDBViewModel;
@@ -40,11 +34,15 @@ public class TeamActivity extends AppCompatActivity {
 
         Log.i(TAG, "reached onActivityResult().");
 
-        teamDBViewModel = ViewModelProviders.of(this).get(TeamDBViewModel.class);
-
         if (requestCode == ADD_PLAYER_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             PlayerEntity playerEntity = (PlayerEntity) data.getExtras().get(AddPlayerActivity.EXTRA_REPLY);
             teamDBViewModel.insert(playerEntity);
+            teamAdapter.setTeam(teamDBViewModel.getAllPlayers().getValue());
+
+            // Test
+            for (PlayerEntity player : teamDBViewModel.getAllPlayers().getValue()) {
+                Log.e(TAG, "PlayerID: " + player.getId());
+            }
         } else {
             // TODO: Exceptionhandling.
         }
@@ -57,7 +55,7 @@ public class TeamActivity extends AppCompatActivity {
 
         // Set up RecyclerView
         teamRecyclerView = (RecyclerView) findViewById(R.id.teamRecyclerView);
-        final TeamAdapter teamAdapter = new TeamAdapter(this);
+        teamAdapter = new TeamAdapter(this);
         teamRecyclerView.setAdapter(teamAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -84,12 +82,5 @@ public class TeamActivity extends AppCompatActivity {
                 startActivityForResult(addPlayerIntent, ADD_PLAYER_ACTIVITY_REQUEST_CODE);
             }
         });
-
-        Intent intent = getIntent();
-        if (intent == null) {
-            Log.i(TAG, "intent was null.");
-        } else {
-            Log.i(TAG, intent.toString());
-        }
     }
 }
