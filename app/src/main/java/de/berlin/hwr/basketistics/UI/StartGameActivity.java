4ab3285f0df1acf75075e91dcd9b16bdeaeba8f1
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-03-04 19:39:18 +0100

    Added popup to starters in StartGameActivity.

--- app/src/main/java/de/berlin/hwr/basketistics/UI/StartGameActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/StartGameActivity.java
@@ -6,12 +6,15 @@ import android.media.AudioTrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -20,14 +23,16 @@ import java.util.concurrent.ExecutionException;
 import de.berlin.hwr.basketistics.Persistency.Entities.MatchEntity;
 import de.berlin.hwr.basketistics.R;
 import de.berlin.hwr.basketistics.ViewModel.MatchesViewModel;
+import de.berlin.hwr.basketistics.ViewModel.TeamViewModel;
 
 public class StartGameActivity extends AppCompatActivity {
 
     public final static String TAG = "StartGameActivity";
-    public final static String STARTERS ="de.berlin.hwr.basketistics.UI.StartGameActivity.STARTERS";
-    public final static String MATCH ="de.berlin.hwr.basketistics.UI.StartGameActivity.MATCH";
+    public final static String STARTERS = "de.berlin.hwr.basketistics.UI.StartGameActivity.STARTERS";
+    public final static String MATCH = "de.berlin.hwr.basketistics.UI.StartGameActivity.MATCH";
 
     private MatchesViewModel matchesViewModel;
+    private TeamViewModel teamViewModel;
 
     // Match
     private TextView opponentTextView;
@@ -43,17 +48,184 @@ public class StartGameActivity extends AppCompatActivity {
     private TextView starter4;
     private TextView starter5;
 
+    // Players popup
+    PopupWindow playerPopupWindow;
+
     private Button startGameButton;
 
-    private void showPlayerPopUp() {}
+    private void showPlayerPopUp() {
+    }
+
+    private void attachPopups() {
+
+        //TODO: Refactor into proper OnClicklistener
+
+        // Make player-listings clickable
+        starter1.setOnClickListener(new View.OnClickListener() {
+
+            RecyclerView playerRecyclerView;
+            TeamAdapter teamAdapter;
+
+            @Override
+            public void onClick(View v) {
+                // Inflate the popup_points.xml View
+                LayoutInflater layoutInflater = StartGameActivity.this.getLayoutInflater();
+                View playerListView = layoutInflater.inflate(R.layout.player_list_popup, null);
+
+                // Create the popup Window
+                playerPopupWindow = new PopupWindow(StartGameActivity.this);
+                playerPopupWindow.setContentView(playerListView);
+                playerPopupWindow.setFocusable(true);
+                // TODO: Calculate from displaysize and pixeldensity!
+                playerPopupWindow.setWidth(1300);
+                playerPopupWindow.showAsDropDown(v);
+
+                // Set up RecyclerView
+                playerRecyclerView =
+                        (RecyclerView) playerPopupWindow.getContentView().findViewById(R.id.playerListRecyclerView);
+                teamAdapter = new TeamAdapter(StartGameActivity.this);
+                teamAdapter.setTeam(teamViewModel.getAllPlayers().getValue());
+                playerRecyclerView.setAdapter(teamAdapter);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StartGameActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                playerRecyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
+        starter2.setOnClickListener(new View.OnClickListener() {
+
+            RecyclerView playerRecyclerView;
+            TeamAdapter teamAdapter;
+
+            @Override
+            public void onClick(View v) {
+                // Inflate the popup_points.xml View
+                LayoutInflater layoutInflater = StartGameActivity.this.getLayoutInflater();
+                View playerListView = layoutInflater.inflate(R.layout.player_list_popup, null);
+
+                // Create the popup Window
+                playerPopupWindow = new PopupWindow(StartGameActivity.this);
+                playerPopupWindow.setContentView(playerListView);
+                playerPopupWindow.setFocusable(true);
+                // TODO: Calculate from displaysize and pixeldensity!
+                playerPopupWindow.setWidth(1300);
+                playerPopupWindow.showAsDropDown(v);
+
+                // Set up RecyclerView
+                playerRecyclerView =
+                        (RecyclerView) playerPopupWindow.getContentView().findViewById(R.id.playerListRecyclerView);
+                teamAdapter = new TeamAdapter(StartGameActivity.this);
+                teamAdapter.setTeam(teamViewModel.getAllPlayers().getValue());
+                playerRecyclerView.setAdapter(teamAdapter);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StartGameActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                playerRecyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
+        starter3.setOnClickListener(new View.OnClickListener() {
+
+            RecyclerView playerRecyclerView;
+            TeamAdapter teamAdapter;
+
+            @Override
+            public void onClick(View v) {
+                // Inflate the popup_points.xml View
+                LayoutInflater layoutInflater = StartGameActivity.this.getLayoutInflater();
+                View playerListView = layoutInflater.inflate(R.layout.player_list_popup, null);
+
+                // Create the popup Window
+                playerPopupWindow = new PopupWindow(StartGameActivity.this);
+                playerPopupWindow.setContentView(playerListView);
+                playerPopupWindow.setFocusable(true);
+                // TODO: Calculate from displaysize and pixeldensity!
+                playerPopupWindow.setWidth(1300);
+                playerPopupWindow.showAsDropDown(v);
+
+                // Set up RecyclerView
+                playerRecyclerView =
+                        (RecyclerView) playerPopupWindow.getContentView().findViewById(R.id.playerListRecyclerView);
+                teamAdapter = new TeamAdapter(StartGameActivity.this);
+                teamAdapter.setTeam(teamViewModel.getAllPlayers().getValue());
+                playerRecyclerView.setAdapter(teamAdapter);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StartGameActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                playerRecyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
+        starter4.setOnClickListener(new View.OnClickListener() {
+
+            RecyclerView playerRecyclerView;
+            TeamAdapter teamAdapter;
+
+            @Override
+            public void onClick(View v) {
+                // Inflate the popup_points.xml View
+                LayoutInflater layoutInflater = StartGameActivity.this.getLayoutInflater();
+                View playerListView = layoutInflater.inflate(R.layout.player_list_popup, null);
+
+                // Create the popup Window
+                playerPopupWindow = new PopupWindow(StartGameActivity.this);
+                playerPopupWindow.setContentView(playerListView);
+                playerPopupWindow.setFocusable(true);
+                // TODO: Calculate from displaysize and pixeldensity!
+                playerPopupWindow.setWidth(1300);
+                playerPopupWindow.showAsDropDown(v);
+
+                // Set up RecyclerView
+                playerRecyclerView =
+                        (RecyclerView) playerPopupWindow.getContentView().findViewById(R.id.playerListRecyclerView);
+                teamAdapter = new TeamAdapter(StartGameActivity.this);
+                teamAdapter.setTeam(teamViewModel.getAllPlayers().getValue());
+                playerRecyclerView.setAdapter(teamAdapter);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StartGameActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                playerRecyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
+        starter5.setOnClickListener(new View.OnClickListener() {
+
+            RecyclerView playerRecyclerView;
+            TeamAdapter teamAdapter;
+
+            @Override
+            public void onClick(View v) {
+                // Inflate the popup_points.xml View
+                LayoutInflater layoutInflater = StartGameActivity.this.getLayoutInflater();
+                View playerListView = layoutInflater.inflate(R.layout.player_list_popup, null);
+
+                // Create the popup Window
+                playerPopupWindow = new PopupWindow(StartGameActivity.this);
+                playerPopupWindow.setContentView(playerListView);
+                playerPopupWindow.setFocusable(true);
+                // TODO: Calculate from displaysize and pixeldensity!
+                playerPopupWindow.setWidth(1300);
+                playerPopupWindow.showAsDropDown(v);
+
+                // Set up RecyclerView
+                playerRecyclerView =
+                        (RecyclerView) playerPopupWindow.getContentView().findViewById(R.id.playerListRecyclerView);
+                teamAdapter = new TeamAdapter(StartGameActivity.this);
+                teamAdapter.setTeam(teamViewModel.getAllPlayers().getValue());
+                playerRecyclerView.setAdapter(teamAdapter);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(StartGameActivity.this);
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                playerRecyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
 
-        // Get MatchesViewModel
+        // Get ViewModels
         matchesViewModel = ViewModelProviders.of(this).get(MatchesViewModel.class);
+        teamViewModel = ViewModelProviders.of(this).get(TeamViewModel.class);
 
         // Find Views
         opponentTextView = findViewById(R.id.start_matchItemOutTeam);
@@ -88,5 +260,6 @@ public class StartGameActivity extends AppCompatActivity {
             startActivity(matchesIntent);
         }
 
+        attachPopups();
     }
 }
