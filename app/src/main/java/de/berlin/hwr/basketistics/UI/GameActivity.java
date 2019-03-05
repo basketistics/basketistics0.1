a1e51aa99e669e90bb5f30091d3a7a6acfa3014c
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-03-05 13:28:24 +0100

    Fixed timer by calling timerHandler() in GameActivity.onCreate().

--- app/src/main/java/de/berlin/hwr/basketistics/UI/GameActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/GameActivity.java
@@ -389,5 +389,6 @@ public class GameActivity extends AppCompatActivity {
         attachPointsPopUp();
         attachButtonsToViewModel();
         attachTextViewsToViewModel();
+        timerHandler();
     }
 }
