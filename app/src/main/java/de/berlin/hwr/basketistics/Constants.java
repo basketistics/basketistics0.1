2742ea100b952eff1b5e395e4e16d5c151f83dd7
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-03-11 11:38:01 +0100

    Added Constants.

--- app/src/main/java/de/berlin/hwr/basketistics/Constants.java
+++ app/src/main/java/de/berlin/hwr/basketistics/Constants.java
@@ -2,18 +2,31 @@ package de.berlin.hwr.basketistics;
 
 public abstract class Constants {
 
-    public static final int POINTS = 10;
-    public static final int ONE_POINT = 11;
-    public static final int TWO_POINTS = 12;
-    public static final int THREE_POINTS = 13;
-    public static final int ONE_POINT_ATTEMPT = 14;
-    public static final int TWO_POINTS_ATTEMPT= 15;
-    public static final int THREE_POINTS_ATTEMPT = 16;
-    public static final int REBOUND = 20;
-    public static final int ASSIST = 30;
-    public static final int STEAL = 40;
-    public static final int BLOCK = 50;
-    public static final int TURNOVER= 60;
-    public static final int FOUL = 70;
+    public static final int GAME_START              =  0;
+    public static final int FIRST_QUARTER_START     =  1;
+    public static final int SECOND_QUARTER_START    =  2;
+    public static final int THIRD_QUARTER_START     =  3;
+    public static final int FOURTH_QUARTER_START    =  4;
+    public static final int FIRST_QUARTER_END       =  5;
+    public static final int SECOND_QUARTER_END      =  6;
+    public static final int THIRD_QUARTER_END       =  7;
+    public static final int FOURTH_QUARTER_END      =  8;
+    public static final int STARTER                 = 10;
+    public static final int IN                      = 11;
+    public static final int OUT                     = 12;
+
+    public static final int POINTS                  = 20;
+    public static final int ONE_POINT               = 21;
+    public static final int TWO_POINTS              = 22;
+    public static final int THREE_POINTS            = 23;
+    public static final int ONE_POINT_ATTEMPT       = 24;
+    public static final int TWO_POINTS_ATTEMPT      = 25;
+    public static final int THREE_POINTS_ATTEMPT    = 26;
+    public static final int REBOUND                 = 30;
+    public static final int ASSIST                  = 40;
+    public static final int STEAL                   = 50;
+    public static final int BLOCK                   = 60;
+    public static final int TURNOVER                = 70;
+    public static final int FOUL                    = 80;
 
 }
