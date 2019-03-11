ae5f3b3bd35f226f47cd27372962a20157474d17
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-03-11 12:22:48 +0100

    Made team image in TeamActivity and MatchesActivity dynamic.

--- app/src/main/java/de/berlin/hwr/basketistics/UI/MatchesActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/MatchesActivity.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
@@ -16,28 +17,29 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.List;
 
+import de.berlin.hwr.basketistics.ImageSaver;
 import de.berlin.hwr.basketistics.Persistency.Entities.MatchEntity;
 import de.berlin.hwr.basketistics.R;
 import de.berlin.hwr.basketistics.ViewModel.MatchesViewModel;
 
 public class MatchesActivity extends AppCompatActivity {
 
-    SharedPreferences sharedPreferences = null;
-
-    private BottomNavigationView bottomNavigationView;
-
-    private MatchesAdapter matchesAdapter;
-
     private final static int ADD_MATCH_ACTIVITY_REQUEST_CODE = 4;
     public static final String TAG = "MatchesActivity";
 
-    private FloatingActionButton addMatchButton;
+    SharedPreferences sharedPreferences = null;
+
     private MatchesViewModel matchesViewModel;
 
+    private FloatingActionButton addMatchButton;
+    private BottomNavigationView bottomNavigationView;
     private RecyclerView matchesRecyclerView;
+    private MatchesAdapter matchesAdapter;
+    private ImageView teamImageView;
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -94,6 +96,14 @@ public class MatchesActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // Set Team Image
+        teamImageView = findViewById(R.id.matchImageView);
+        ImageSaver imageSaver = new ImageSaver(this);
+        Bitmap teamBitmap =  imageSaver.setDirectoryName("images")
+                .setFileName(sharedPreferences.getString("team_image", ""))
+                .load();
+        teamImageView.setImageBitmap(teamBitmap);
 
         // Set up RecyclerView
         matchesRecyclerView = (RecyclerView) findViewById(R.id.matchesRecyclerView);
