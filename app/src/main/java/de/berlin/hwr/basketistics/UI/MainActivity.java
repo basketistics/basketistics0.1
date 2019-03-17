7d6d5a3519217504e78989a1803a5de0545f8041
Jona Heitzer <s_heitzer@stud.hwr-berlin.de>
2019-03-17 17:57:16 +0100

    Added TestReportFragment and made teamImage go away when it is visible.

--- app/src/main/java/de/berlin/hwr/basketistics/UI/MainActivity.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.Guideline;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -28,9 +30,13 @@ import java.util.Date;
 import de.berlin.hwr.basketistics.R;
 import de.berlin.hwr.basketistics.UI.Fragments.MatchesFragment;
 import de.berlin.hwr.basketistics.UI.Fragments.TeamFragment;
+import de.berlin.hwr.basketistics.UI.Fragments.TestReportsFragment;
 
-public class MainActivity extends AppCompatActivity
-        implements MatchesFragment.OnFragmentInteractionListener, TeamFragment.OnFragmentInteractionListener {
+public class MainActivity
+        extends AppCompatActivity
+        implements MatchesFragment.OnFragmentInteractionListener,
+        TeamFragment.OnFragmentInteractionListener,
+        TestReportsFragment.OnFragmentInteractionListener {
 
     private static final String TAG = "MainActivity";
     private static final int PICK_TEAM_IMAGE = 13;
@@ -41,10 +47,23 @@ public class MainActivity extends AppCompatActivity
     private Bitmap teamBitmap;
 
     private ImageView teamImageView;
+    private Guideline guideline;
     private SectionsStatePagerAdapter sectionsStatePagerAdapter;
     private ViewPager viewPager;
     private BottomNavigationView bottomNavigationView;
     private MenuItem prevMenuItem;
+
+    public void hideTeamImage() {
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) guideline.getLayoutParams();
+        params.guidePercent = 0.00f;
+        guideline.setLayoutParams(params);
+    }
+
+    public void showTeamImage() {
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) guideline.getLayoutParams();
+        params.guidePercent = 0.33f;
+        guideline.setLayoutParams(params);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,9 +115,7 @@ public class MainActivity extends AppCompatActivity
                         setViewPager(1);
                         break;
                     case R.id.reports:
-                        Intent reportsIntent = new Intent(MainActivity.this, ReportActivity.class);
-                        startActivity(reportsIntent);
-                        overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out);
+                        setViewPager(2);
                         break;
                     default:
                         return false;
@@ -110,6 +127,7 @@ public class MainActivity extends AppCompatActivity
 
         // Set Team Image
         teamImageView = findViewById(R.id.mainTeamImage);
+        guideline = findViewById(R.id.mainGuideLine);
 
         File directory = this.getDir("images", Context.MODE_PRIVATE);
         File image = new File(directory, teamImageFilename);
@@ -129,6 +147,7 @@ public class MainActivity extends AppCompatActivity
                 return false;
             }
         });
+
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int i, float v, int i1) {
@@ -142,6 +161,13 @@ public class MainActivity extends AppCompatActivity
                 } else {
                     bottomNavigationView.getMenu().getItem(0).setChecked(false);
                 }
+
+                if (position == 2) {
+                    hideTeamImage();
+                } else {
+                    showTeamImage();
+                }
+
                 Log.d("page", "onPageSelected: " + position);
                 bottomNavigationView.getMenu().getItem(position).setChecked(true);
                 prevMenuItem = bottomNavigationView.getMenu().getItem(position);
@@ -151,6 +177,7 @@ public class MainActivity extends AppCompatActivity
             public void onPageScrollStateChanged(int i) {
 
             }
+
         });
     }
 
@@ -200,6 +227,7 @@ public class MainActivity extends AppCompatActivity
         SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new TeamFragment(), "TeamFragment"); // item #0
         adapter.addFragment(new MatchesFragment(), "MatchesFragment"); // item #1
+        adapter.addFragment(new TestReportsFragment(), "ReportsFragment"); // item #2
         viewPager.setAdapter(adapter);
     }
 
@@ -210,6 +238,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
         Log.i(TAG, "onFragmentInteraction: was called.");
+        Log.e(TAG, "onFragmentInteraction: " + uri.getEncodedFragment());
     }
 
     public String getTeamName() {
