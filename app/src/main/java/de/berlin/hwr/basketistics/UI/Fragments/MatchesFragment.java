4aca4036b73c6e2a01bd207a7357ab2083886c1d
Boris Naefe <Boris.Naefe@deutschebahn.com>
2019-03-18 02:45:17 +0100

    gamereport after game implemented

--- app/src/main/java/de/berlin/hwr/basketistics/UI/Fragments/MatchesFragment.java
+++ app/src/main/java/de/berlin/hwr/basketistics/UI/Fragments/MatchesFragment.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ import de.berlin.hwr.basketistics.UI.MainActivity;
 import de.berlin.hwr.basketistics.UI.Fragments.Adapter.MatchesAdapter;
 import de.berlin.hwr.basketistics.UI.OnMatchReportClickedListener;
 import de.berlin.hwr.basketistics.UI.StartGameActivity;
+import de.berlin.hwr.basketistics.ViewModel.GameReportViewModel;
 import de.berlin.hwr.basketistics.ViewModel.MatchesViewModel;
 
 import static android.app.Activity.RESULT_OK;
@@ -62,8 +64,25 @@ public class MatchesFragment extends Fragment implements OnMatchReportClickedLis
     private FloatingActionButton addMatchButton;
 
     private MatchesViewModel matchesViewModel;
+    private GameReportViewModel gameReportViewModel;
     private static String teamName;
     private boolean showLastGame = false;
+
+
+    private String[] parseTextViewItems(String[] list){
+
+        String[] parsedList = new String[3];
+        for (int i =0; i<3;i++){
+            if (list[i].length()>=12)
+                parsedList[i] = (list[i].substring(0,12)+"%");
+            else
+                parsedList[i]=list[i]+"%";
+
+        }
+
+        return parsedList;
+    }
+
 
     public MatchesFragment() {
         // Required empty public constructor
@@ -155,11 +174,88 @@ public class MatchesFragment extends Fragment implements OnMatchReportClickedLis
             popupWindow.setClippingEnabled(false);
             popupWindow.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
             popupWindow.setHeight(LinearLayout.LayoutParams.MATCH_PARENT);
-            popupWindow.setBackgroundDrawable();
             popupWindow.showAtLocation(matchesRecyclerView, 0, 0, 0);
             ((MainActivity)getActivity()).hideTeamImage();
 
             // TODO: Logic.
+            gameReportViewModel = ViewModelProviders.of(this).get(GameReportViewModel.class);
+
+
+            //-------TextViews--------
+
+            TextView pointsMade = matchesRecyclerView.findViewById(R.id.visu_points_valG);
+            TextView freeThrows = matchesRecyclerView.findViewById(R.id.visu_fts_valG);
+            TextView fieldGoals = matchesRecyclerView.findViewById(R.id.visu_fgs_valG);
+            TextView fieldGoals3 = matchesRecyclerView.findViewById(R.id.visu_fgs3_valG);
+            TextView rebounds = matchesRecyclerView.findViewById(R.id.visu_rebounds_valG);
+            TextView assists = matchesRecyclerView.findViewById(R.id.visu_assists_valG);
+            TextView blocks = matchesRecyclerView.findViewById(R.id.visu_blocks_valG);
+            TextView steals = matchesRecyclerView.findViewById(R.id.visu_steals_valG);
+            TextView turnover = matchesRecyclerView.findViewById(R.id.visu_tov_valG);
+            TextView fouls = matchesRecyclerView.findViewById(R.id.visu_fouls_valG);
+
+
+            int matchId = ((MainActivity)getActivity()).getLastMatchId();
+
+            GameReportViewModel.GameReport gameReport = gameReportViewModel.getGameReport(matchId);
+            String[] inputList = new String[3];
+            inputList[0] = gameReport.onePoint+ "/ " + gameReport.onePointAttempt +"/ " + ((100/(float)gameReport.onePointAttempt)*(float)gameReport.onePoint);
+            inputList[1] = (gameReport.twoPoints+ "/ " + gameReport.twoPointsAttempt +"/ " + ((100/(float)gameReport.twoPointsAttempt)*(float)gameReport.twoPoints));
+            inputList[2] = gameReport.threePoints+ "/ " + gameReport.threePointsAttempt +"/ " + ((100/(float)gameReport.threePointsAttempt)*(float)gameReport.threePoints);
+
+            String[] textlist = parseTextViewItems(inputList);
+
+            String pointsMadeText = ((float)gameReport.onePoint + (float)gameReport.twoPoints*2 + (float)gameReport.threePoints*3)+"";
+
+            pointsMadeText = (pointsMadeText.length()>4) ? pointsMadeText.replaceAll(" ","").substring(0,4) :  pointsMadeText ;
+
+
+            pointsMade.setText(pointsMadeText);
+            if (gameReport.onePointAttempt == 0)
+                freeThrows.setText("0/ 0/ 0%");
+            else
+                freeThrows.setText(textlist[0]);
+            if (gameReport.twoPointsAttempt == 0)
+                fieldGoals.setText("0/ 0/ 0%");
+            else
+                fieldGoals.setText(textlist[1]);
+            if (gameReport.threePointsAttempt == 0)
+                fieldGoals3.setText("0/ 0/ 0%");
+            else
+                fieldGoals3.setText(textlist[2]);
+
+            String rebText = (float) gameReport.rebound + "";
+            rebText = rebText.length() > 5 ? rebText.substring(0, 4) : rebText;
+            rebounds.setText(rebText);
+
+            String astText = ((float) gameReport.assist + "");
+            astText = astText.length() > 5 ? astText.substring(0, 4) : astText;
+            assists.setText(astText);
+
+            String blkText = (float) gameReport.block + "";
+            blkText = blkText.length() > 5 ? blkText.substring(0, 4) : blkText;
+            blocks.setText(blkText);
+
+            String stlText = (float) gameReport.steal + "";
+            stlText = stlText.length() > 5 ? stlText.substring(0, 4) : stlText;
+            steals.setText(stlText);
+
+            String tovText = (float) gameReport.turnover + "";
+            tovText = tovText.length() > 5 ? tovText.substring(0, 4) : tovText;
+            turnover.setText(tovText);
+
+            String foulText = (float) gameReport.foul + "";
+            foulText = foulText.length() > 5 ? foulText.substring(0, 4) : foulText;
+            fouls.setText(foulText);
+
+
+
+
+
+
+
+
+
 
             playerListView.setOnClickListener(new View.OnClickListener() {
                 @Override
